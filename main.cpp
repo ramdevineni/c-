f842bf65ea2a3b79658fa951bd47c5e0c4a9bbf8
ramdevineni <54804353+ramdevineni@users.noreply.github.com>
2019-09-13 19:37:09 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+
+const int MOD =1000000007;
+
+long long noofways(int n,int k, int x )
+{
+  long long b[n],a[n];
+
+    a[0]=x==1?1:0;
+    b[0]=x==1?0:1;
+     for(int y=1;y<n;++y)
+       {
+           a[y]=b[y-1]%MOD;
+           b[y]=(a[y-1]*(k-1)+b[y-1]*(k-2))%MOD;
+       }
+    return a[n-1];
+
+}
+
+int main()
+{
+    int n,k,x;
+    cin>>n>>k>>x;
+    cout<<noofways(n,k,x);
+
+
+    return 0;
+}
